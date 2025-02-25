a6ea1c06ce3b1992b50bc611f228b96f040de705
ScrappyNozicka <vojtech2012@gmail.com>
2025-02-25 15:58:52 +0000

    extract/get_data in progress, timestamp inclusion testing needed as well as more robust func testing --- next will require get_timestamp_from_s3 function and testing

--- src/extract/extract_utils/get_data_from_db.py
+++ src/extract/extract_utils/get_data_from_db.py
@@ -1,7 +1,12 @@
-from src.extract.extract_utils.connection import create_conn, close_conn
+from src.extract.extract_utils.connection import create_conn
 from pprint import pprint
+from pg8000.exceptions import DatabaseError
+from datetime import timedelta
 
-def get_data_from_db(query):
+class ConnectionError(Exception):
+    pass
+
+def get_data_from_db(s3_timestamp=None):
     """Handles data extraction from the initial data-source database.
 
     On reception of the data we pass them on for processing and selection based on the datetime datastamp.
@@ -20,21 +25,20 @@ def get_data_from_db(query):
     Raises:
         ConnectionError: Database connection not available.
     """    
-    db = create_conn()
-    table_names = ["counterparty", "currency", "department", "design", "staff", "sales_order", "address", "payment", "purchase_order", "payment_type", "transaction"]
-    result = []
-    for table in table_names:
-        query_result = db.run("SELECT * FROM table;")
-        columns = [col['name'] for col in db.columns]
-        result.append([dict(zip(columns, result)) for result in query_result])
-    db.close()
-    pprint(result)
-    return result
-
-    #pass in the create connection
-    #create list of table names
-    #iterate over each table to get the data
-    #db run to select all from the table if timestamp none
-    #if timestamp, select only after the timestamp
-    #pass in the close connection
-    #return all the data selected
+    try:
+        db = create_conn()
+        table_names = ["counterparty", "currency", "department", "design", "staff", "sales_order", "address", "payment", "purchase_order", "payment_type", "transaction"]
+        result = {}
+        query_minutes_str = ';'
+        if s3_timestamp != None:
+            query_minutes_str = f' where created_at > {s3_timestamp} \n'
+            f' or last_updated > {s3_timestamp};'
+        for table in table_names:
+            query_result = db.run("SELECT * FROM table" + query_minutes_str)
+            columns = [col['name'] for col in db.columns]
+            result[table] = [dict(zip(columns, result)) for result in query_result]
+        db.close()
+        return result
+    except Exception as err:
+        print("Error: Database connection not found", err)
+        raise ConnectionError
