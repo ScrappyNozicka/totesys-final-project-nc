efff412187c7d062aeb9e40179fccfa33e04590c
yanrongzhang1 <graceyg97@gmail.com>
2025-02-27 21:37:13 +0000

    integrate extract python code lambda function and terraform

--- terraform/extract_tf/layer.tf
+++ terraform/extract_tf/layer.tf
@@ -1,13 +1,21 @@
 data "archive_file" "lambda_package" {
  type        = "zip"
- source_file  = "${path.module}/extract_lambda.tf"
- output_path = "${path.module}/extract_lambda.zip"
+ source_dir  = "${path.module}/../../src/extract"
+ output_path = "${path.module}/../extract_lambda.zip"
 }
 
 
-resource "aws_lambda_layer_version" "extract_layer" {
-    filename = data.archive_file.lambda_package.output_path
-    layer_name = "extract-layer"
+data "archive_file" "layer" {
+  type = "zip"
+  output_file_mode = "0666"
+  source_dir = "${path.module}/layer/"
+  output_path = "${path.module}/../extract_layer.zip"
+  
+}
+
+resource "aws_lambda_layer_version" "extract_python_layer" {
+    filename = data.archive_file.layer.output_path
+    layer_name = "extract_python_layer"
     compatible_runtimes=["python3.13"]
     
 }
