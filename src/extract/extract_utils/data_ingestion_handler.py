6ccb54c51cf09d2a05c9f457ea5b81228ee8bb4d
Miigget <marcinsdl@icloud.com>
2025-02-28 16:36:48 +0000

    refactor - ready to merge

--- src/extract/extract_utils/data_ingestion_handler.py
+++ src/extract/extract_utils/data_ingestion_handler.py
@@ -1,4 +1,5 @@
 import json
+from decimal import Decimal
 from extract_utils.totesys_processor import ToteSysProcessor
 from extract_utils.s3_file_handler import S3FileHandler
 
@@ -23,35 +24,18 @@ class DataIngestionHandler:
         for table_name in table_names:
             # get list of rows in current table
             table_data = totesys_data[table_name]
-            rows = []
+
             last_updated_max = ""
             for row in table_data:
                 last_updated = self.processor.get_last_updated(row)
                 if last_updated > last_updated_max:
                     last_updated_max = last_updated
-                row_json = json.dumps(
-                    row, default=self.processor.decimal_to_str
-                )
-                rows.append(row_json)
+                for key, value in row.items():
+                    if isinstance(value, Decimal):
+                        row[key] = float(value)
             
-            file_data = json.dumps(rows)
+            file_data = json.dumps(table_data)
 
             file_name = self.s3_handler.get_new_file_name(table_name, last_updated_max)
 
             self.s3_handler.upload_file(file_data, file_name)
-            print(table_name, "<--- UPLOADED SUCCESSFULLY")
-
-
-
-
-            # for row in table_data:
-            #     row_id = self.processor.get_row_id(row, table_name)
-            #     last_updated = self.processor.get_last_updated(row)
-            #     file_data = json.dumps(
-            #         row, default=self.processor.decimal_to_str
-            #     )
-            #     file_name = self.s3_handler.get_new_file_name(
-            #         table_name, row_id, last_updated
-            #     )
-
-            #     self.s3_handler.upload_file(file_data, file_name)
