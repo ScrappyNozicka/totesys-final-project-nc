faa7801f7bdf6c3811eb5b19ac20063d93f0b739
ScrappyNozicka <vojtech2012@gmail.com>
2025-03-10 15:22:47 +0000

    email alert setup for transform and load stage

--- terraform/etl_tf/cloudwatch.tf
+++ terraform/etl_tf/cloudwatch.tf
@@ -6,25 +6,69 @@ topic_arn = aws_sns_topic.terraform_alerts.arn
 protocol  = "email"
 endpoint  = "kettslough@gmail.com"
 }
-resource "aws_cloudwatch_log_metric_filter" "error_filter" {
-name = "ErrorFilter"
+resource "aws_cloudwatch_log_metric_filter" "extract_error_filter" {
+name = "ExtractErrorFilter"
 log_group_name = "/aws/lambda/${var.extract_lambda_name}"
 pattern = "ERROR"
 metric_transformation {
-   name = "TerraformErrorCount"
+   name = "ExtractErrorCount"
    namespace = "TerraformErrors"
    value = "1"
 }
 }
-resource "aws_cloudwatch_metric_alarm" "terraform_error_alarm" {
- alarm_name          = "TerraformErrorAlarm"
+resource "aws_cloudwatch_metric_alarm" "extract_error_alarm" {
+ alarm_name          = "ExtractErrorAlarm"
  comparison_operator = "GreaterThanOrEqualToThreshold"
  evaluation_periods  = 1
- metric_name         = "TerraformErrorCount"
+ metric_name         = "ExtractErrorCount"
  namespace           = "TerraformErrors"
  period              = 60
  statistic           = "Sum"
  threshold           = 1
- alarm_description   = "Alarm when Lambda function logs an ERROR"
+ alarm_description   = "Alarm when Extract Lambda function logs an ERROR"
+ alarm_actions       = [aws_sns_topic.terraform_alerts.arn]
+}
+resource "aws_cloudwatch_log_metric_filter" "transform_error_filter" {
+name = "TransformErrorFilter"
+log_group_name = "/aws/lambda/${var.transform_lambda_name}"
+pattern = "ERROR"
+metric_transformation {
+   name = "TransformErrorCount"
+   namespace = "TerraformErrors"
+   value = "1"
+}
+}
+resource "aws_cloudwatch_metric_alarm" "transform_error_alarm" {
+ alarm_name          = "TransformErrorAlarm"
+ comparison_operator = "GreaterThanOrEqualToThreshold"
+ evaluation_periods  = 1
+ metric_name         = "TransformErrorCount"
+ namespace           = "TerraformErrors"
+ period              = 60
+ statistic           = "Sum"
+ threshold           = 1
+ alarm_description   = "Alarm when Transform Lambda function logs an ERROR"
+ alarm_actions       = [aws_sns_topic.terraform_alerts.arn]
+}
+resource "aws_cloudwatch_log_metric_filter" "load_error_filter" {
+name = "LoadErrorFilter"
+log_group_name = "/aws/lambda/${var.load_lambda_name}"
+pattern = "ERROR"
+metric_transformation {
+   name = "LoadErrorCount"
+   namespace = "TerraformErrors"
+   value = "1"
+}
+}
+resource "aws_cloudwatch_metric_alarm" "load_error_alarm" {
+ alarm_name          = "LoadErrorAlarm"
+ comparison_operator = "GreaterThanOrEqualToThreshold"
+ evaluation_periods  = 1
+ metric_name         = "LoadErrorCount"
+ namespace           = "TerraformErrors"
+ period              = 60
+ statistic           = "Sum"
+ threshold           = 1
+ alarm_description   = "Alarm when Load Lambda function logs an ERROR"
  alarm_actions       = [aws_sns_topic.terraform_alerts.arn]
 }
